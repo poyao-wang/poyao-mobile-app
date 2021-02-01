9186a9a9434b4811dedd9561d2de0bbfdec98a16
Poyao Wang <michael90110@gmail.com>
2021-02-01 22:55:39 +0900

    ProfileScreen dummy data

--- app/screen/ProfileScreen.js
+++ app/screen/ProfileScreen.js
@@ -2,9 +2,27 @@ import React from "react";
 import { View, StyleSheet, Text } from "react-native";
 
 function ProfileScreen(props) {
+  const thisYear = new Date();
+  const myAge = thisYear.getFullYear() - 1988;
+
   return (
     <View style={styles.container}>
-      <Text>Profile Screen</Text>
+      <Text>Profile</Text>
+      <Text>A Self-Taught Programmer.</Text>
+      <Text>About me</Text>
+      <Text>
+        I am a mechanical engineer. Working in automobile industry. I started
+        stydying programming since 2019.
+      </Text>
+      <Text>Details</Text>
+      <Text>Name:</Text>
+      <Text>Poyao Wang</Text>
+      <Text>Nickname:</Text>
+      <Text>Boyo</Text>
+      <Text>Age:</Text>
+      <Text>{myAge + " years"}</Text>
+      <Text>Location:</Text>
+      <Text>Tokyo, Japan</Text>
     </View>
   );
 }
