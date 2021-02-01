6b2663c89656afd5c75c431746a0eb99846422d2
Poyao Wang <michael90110@gmail.com>
2021-02-01 23:48:06 +0900

    Add table and dummy data into AbilityScreen

--- app/screen/AbilityScreen.js
+++ app/screen/AbilityScreen.js
@@ -1,10 +1,47 @@
 import React from "react";
 import { View, StyleSheet, Text } from "react-native";
+import AbilityTable from "../components/AbilityTable";
 
 function AbilityScreen(props) {
+  const data = {
+    skills: [
+      { name: "JavaScript", rate: 4 },
+      { name: "ReactJS", rate: 3 },
+      { name: "Python", rate: 3 },
+      { name: "Bootstrap", rate: 3 },
+      { name: "HTML", rate: 3 },
+      { name: "CSS", rate: 3 },
+      { name: "VBA", rate: 3 },
+      { name: "node.js", rate: 3 },
+      { name: "Cmd lines", rate: 2 },
+      { name: "Google Cloud", rate: 2 },
+    ],
+    languages: [
+      { name: "Chinese", rate: 5 },
+      { name: "English", rate: 4 },
+      { name: "Japanese", rate: 4 },
+      { name: "German", rate: 1 },
+    ],
+    tools: [
+      { name: "MS Office", rate: 5 },
+      { name: "Mac", rate: 5 },
+      { name: "Windows", rate: 5 },
+      { name: "MS VSCode", rate: 3 },
+      { name: "Adobe Ai", rate: 3 },
+      { name: "Adobe Ps", rate: 3 },
+      { name: "Fusion 360", rate: 3 },
+      { name: "Google Drive", rate: 3 },
+      { name: "Raspberry Pi", rate: 3 },
+      { name: "Git / Github", rate: 2 },
+    ],
+  };
+
   return (
     <View style={styles.container}>
       <Text>Ability Screen</Text>
+      <AbilityTable title="Skills" data={data.skills} />
+      <AbilityTable title="languages" data={data.languages} />
+      <AbilityTable title="tools" data={data.tools} />
     </View>
   );
 }
