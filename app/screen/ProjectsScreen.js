1382e2a84fef804503c5f0091a323216614188f7
Poyao Wang <michael90110@gmail.com>
2021-02-02 21:25:14 +0900

    Added dummy data and table into ProjectScreen

--- app/screen/ProjectsScreen.js
+++ app/screen/ProjectsScreen.js
@@ -1,10 +1,35 @@
 import React from "react";
 import { View, StyleSheet, Text } from "react-native";
+import ProjectTable from "../components/ProjectTable";
 
 function ProjectsScreen(props) {
+  const data = {
+    cards: [
+      {
+        imgSrc: "assets/Project1.jpg",
+        cardTitleText: "This web app / ReactJS",
+        cardText:
+          "This webpage is built by reactJS, combined with " +
+          "Bootstrap, and deployed on Google Cloud.",
+        btnLink: "https://github.com/BoyoWang/my-web-app",
+        btnText: "Github link",
+      },
+      {
+        imgSrc: "assets/Project2.jpg",
+        cardTitleText: "Raspberry Pi / Python",
+        cardText:
+          "A motor controller, built on Raspberry Pi, " +
+          "coding in Python, using Tkinter as GUI.",
+        btnLink: "https://github.com/BoyoWang/control-box",
+        btnText: "Github link",
+      },
+    ],
+  };
+
   return (
     <View style={styles.container}>
       <Text>Projects Screen</Text>
+      <ProjectTable data={data.cards} />
     </View>
   );
 }
