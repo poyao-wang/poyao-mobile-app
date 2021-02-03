54a4be33d9c98b7735ffaceb7b4051d1bafe220c
Poyao Wang <michael90110@gmail.com>
2021-02-03 17:04:28 +0900

    Make ProfileScreen contents to an  object

--- app/screen/ProfileScreen.js
+++ app/screen/ProfileScreen.js
@@ -3,33 +3,37 @@ import Text from "../components/Text";
 import ScreenForApp from "../components/ScreenForApp";
 import { View, StyleSheet, Image } from "react-native";
 import TextH2 from "../components/TextH2";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 function ProfileScreen(props) {
+  const data = {
+    oneLineMsg: "A Self-Taught Programmer",
+    aboutMe:
+      "I am a mechanical engineer. Working in automobile industry. I started stydying programming since 2019.",
+    details: [
+      { title: "Age :", content: "33 years" },
+      { title: "Location :", content: "Tokyo, Japan" },
+    ],
+  };
   const thisYear = new Date();
   const myAge = thisYear.getFullYear() - 1988;
 
   return (
     <ScreenForApp mainTitle="Profile">
       <View style={styles.content}>
-        <TextH2>About me</TextH2>
-        <Text style={styles.oneLineMsg}>A Self-Taught Programmer.</Text>
-        <Text>
-          I am a mechanical engineer. Working in automobile industry. I started
-          stydying programming since 2019.
-        </Text>
-
+        <Text style={styles.oneLineMsg}>{data.oneLineMsg}</Text>
+        <Text>{data.aboutMe}</Text>
         <Image
           source={require("../assets/me2.jpeg")}
           style={styles.myPicture}
         />
-
         <TextH2>Details</TextH2>
         <Text style={styles.myName}>Poyao Wang</Text>
-        <Text style={styles.detailTitle}>Age:</Text>
-        <Text>{myAge + " years"}</Text>
-        <Text style={styles.detailTitle}>Location:</Text>
-        <Text>Tokyo, Japan</Text>
+        {data.details.map((rowData, key) => (
+          <React.Fragment key={key}>
+            <Text style={styles.detailTitle}>{rowData.title}</Text>
+            <Text>{rowData.content}</Text>
+          </React.Fragment>
+        ))}
       </View>
     </ScreenForApp>
   );
@@ -55,6 +59,7 @@ const styles = StyleSheet.create({
   oneLineMsg: {
     fontSize: 20,
     marginBottom: 7,
+    fontWeight: "500",
   },
 });
 
