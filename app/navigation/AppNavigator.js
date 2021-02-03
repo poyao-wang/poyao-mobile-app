77b17742f95a0cbbb5087e43304a8b40713c49bc
Poyao Wang <michael90110@gmail.com>
2021-02-03 16:46:45 +0900

    Add icons to AppNavigator

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -1,10 +1,10 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import React from "react";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 import AbilityScreen from "../screen/AbilityScreen";
 import ContactScreen from "../screen/ContactScreen";
 import ExperienceScreen from "../screen/ExperienceScreen";
-import HomeScreen from "../screen/HomeScreen";
 import ProfileScreen from "../screen/ProfileScreen";
 import ProjectsScreen from "../screen/ProjectsScreen";
 
@@ -13,11 +13,71 @@ const Tab = createBottomTabNavigator();
 function AppNavigator() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Profile" component={ProfileScreen} />
-      <Tab.Screen name="Experience" component={ExperienceScreen} />
-      <Tab.Screen name="Contact" component={ContactScreen} />
-      <Tab.Screen name="Ability" component={AbilityScreen} />
-      <Tab.Screen name="Projects" component={ProjectsScreen} />
+      <Tab.Screen
+        name="Profile"
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons //
+              name="account"
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+        component={ProfileScreen}
+      />
+      <Tab.Screen
+        name="Experience"
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons //
+              name="bulletin-board"
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+        component={ExperienceScreen}
+      />
+      <Tab.Screen
+        name="Ability"
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons //
+              name="school"
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+        component={AbilityScreen}
+      />
+      <Tab.Screen
+        name="Projects"
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons //
+              name="folder-multiple"
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+        component={ProjectsScreen}
+      />
+      <Tab.Screen
+        name="Contact"
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons //
+              name="phone"
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+        component={ContactScreen}
+      />
     </Tab.Navigator>
   );
 }
