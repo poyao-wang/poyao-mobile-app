743d6fd1b84e79d4623e8676c03815c26bafb70d
Poyao Wang <michael90110@gmail.com>
2021-02-06 23:03:21 +0900

    Beartify ProjectScreen contents

--- app/components/ProjectTableRow.js
+++ app/components/ProjectTableRow.js
@@ -1,29 +1,60 @@
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import {
+  View,
+  StyleSheet,
+  Button,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import * as Linking from "expo-linking";
 import React from "react";
-import { View, StyleSheet, Button, Image } from "react-native";
+import * as Animatable from "react-native-animatable";
+
+import colors from "../config/colors";
 import Text from "./Text";
 import TextH3 from "./TextH3";
-import * as Linking from "expo-linking";
-import colors from "../config/colors";
 import TextH4 from "./TextH4";
 import TextH5 from "./TextH5";
 
 function ProjectTableRow({ rowData }) {
   const linkPress = (url) => {
-    Linking.openURL(url);
+    if (url) Linking.openURL(url);
   };
-
+  const blink = {
+    0: {
+      scale: 1,
+    },
+    0.5: {
+      scale: 1.05,
+    },
+    1: {
+      scale: 1,
+    },
+  };
   return (
     <View style={styles.card}>
       <Image source={{ uri: rowData.imgSrc }} style={styles.myPicture} />
       <View style={styles.container}>
         <TextH5 style={styles.jobTitle}>{rowData.cardTitleText}</TextH5>
-        <Text>{rowData.cardText}</Text>
-        <Button
-          title={rowData.btnText}
-          onPress={() => {
-            linkPress(rowData.btnLink);
-          }}
-        />
+        <Text style={{ marginBottom: 30 }}>{rowData.cardText}</Text>
+        <Animatable.View
+          animation={blink}
+          duration={300}
+          iterationDelay={4000}
+          iterationCount="infinite"
+          style={styles.githubLinkBtn}
+        >
+          <TouchableOpacity
+            onPress={() => {
+              linkPress(rowData.btnLink);
+            }}
+          >
+            <Text style={styles.githubLinkBtnText}>
+              <MaterialCommunityIcons name="github" size={24} />
+              {" GitHub Link"}
+            </Text>
+          </TouchableOpacity>
+        </Animatable.View>
       </View>
     </View>
   );
@@ -45,6 +76,23 @@ const styles = StyleSheet.create({
   },
   container: {
     padding: 10,
+    paddingBottom: 20,
+  },
+  githubLinkBtn: {
+    borderWidth: 0,
+    alignSelf: "center",
+    justifyContent: "center",
+    backgroundColor: colors.primary,
+    padding: 5,
+    paddingTop: 8,
+    paddingHorizontal: 20,
+    borderRadius: 15,
+  },
+  githubLinkBtnText: {
+    alignSelf: "center",
+    fontSize: 23,
+    color: colors.white,
+    fontWeight: "500",
   },
 });
 export default ProjectTableRow;
