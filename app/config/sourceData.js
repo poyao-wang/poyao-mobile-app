8234783b97a755b9430e97adc686f76ff3f78f20
Poyao Wang <michael90110@gmail.com>
2021-02-07 18:18:10 +0900

    add sourceData.js

---
+++ app/config/sourceData.js
@@ -0,0 +1,23 @@
+export default {
+  eng: {
+    home: {
+      title: "Poyao  Wang",
+      subTitle: "This is the mobile app of Poyao.",
+      buttonTitle: "E N T E R",
+    },
+  },
+  cht: {
+    home: {
+      title: "王 柏 堯",
+      subTitle: "這是王柏堯的個人App",
+      buttonTitle: "E N T E R",
+    },
+  },
+  jpn: {
+    home: {
+      title: "ワン　ポヤオ",
+      subTitle: "ワンポヤオのモバイルアプリ",
+      buttonTitle: "E N T E R",
+    },
+  },
+};
